79e6db68391be37c9da997d3283e844009131c70
Zachary Bennett <zbennett10@hotmail.com>
2017-06-27 11:48:09 -0700

    added initial ideas for bson differ

---
+++ diff_bson.pl
@@ -0,0 +1,9 @@
+#!/bin/perl
+use strict;
+use warnings;
+
+#take in a path to a bson file and a path to a json file
+#bsondump the bson file to a json file
+#create an empty hash table
+#find the differences between the files by line
+#for each difference, add a new hash to the hash table where the key is the difference number (0,1,2) and the value is an array of hash tables [firstLine#: firstlineText, secondLine#: secondLineText]
