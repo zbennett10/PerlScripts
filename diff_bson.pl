450b30a4e1f23950b75eb4c91405f77e166a577a
Zachary Bennett <zbennett10@hotmail.com>
2017-07-02 11:21:02 -0700

    naive approach at diffing txt files

--- diff_bson.pl
+++ diff_bson.pl
@@ -1,9 +1,53 @@
 #!/bin/perl
 use strict;
 use warnings;
+use List::Util qw[min max];
 
 #take in a path to a bson file and a path to a json file
 my ($bsonPath, $jsonPath) = @ARGV;
+
+#bsondump the bson file to a json file
+system("bsondump $bsonPath > output.json");
+open my $outputJSON , '<', 'output.json';
+my @bsonLines;
+while(my $line = <$outputJSON>) {
+    chomp($line);
+    $line =~ s/\s//g;
+    push(@bsonLines, $line);
+}
+
+open my $jsonFile , '<', $jsonPath or die "Couldn't open";
+my @jsonLines;
+while(my $line = <$jsonFile>) {
+    chomp($line);
+    $line =~ s/\s//g;
+    push(@jsonLines, $line);
+}
+
+close $outputJSON;
+close $jsonFile;
+
+my $bsonLineLength = scalar @bsonLines;
+my $jsonLineLength = scalar @jsonLines;
+my $checkLength = max($bsonLineLength, $jsonLineLength);
+
+for(my $i = 0; $i < $checkLength; $i++) {
+    my $bsonLine = $bsonLines[$i];
+    my $jsonLine = $jsonLines[$i];
+    if($bsonLine ne $jsonLine && length($bsonLine) > length($jsonLine)) {
+        print $i + 1, ' ++++'.$bsonLine."\n";
+        print $i + 1, ' ----'.$jsonLine."\n";
+    }
+
+    if($bsonLine ne $jsonLine && length($jsonLine) > length($bsonLine)) {
+        print $i + 1, ' ++++'.$jsonLine."\n";
+        print $i + 1, ' ----'.$bsonLine."\n";
+        print "\n";
+    }
+}
+
+#fix the above to determine if records were removed and display the removed records
+#determine which records were changed and display the changed records
 
 
 #bsondump the bson file to a json file
