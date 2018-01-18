8f33f8e70eeeb927eca9db82cca4115ab0a04d83
Zachary Bennett <bennett.zachary@outlook.com>
2018-01-18 14:55:52 -0800

    added custom kml to csv converter

---
+++ kml_to_csv.pl
@@ -0,0 +1,102 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use XML::LibXML;
+use Class::CSV;
+use Data::Dumper;
+
+my ($kmlFileName, $csvFileName) = @ARGV;
+
+# my $csv = Class::CSV->new(
+#     fields => [qw/x y altitude name dtdid fix_rrr_ddd remarks latitude longitude exp_alt time_from_to time_to_td c1 c2 c3 c4/]
+# );
+
+my $kmlFile = XML::LibXML->new->parse_file($kmlFileName);
+my @waypoints;
+
+my @x_y_alt_lines;
+my @name_lines;
+my @data_lines;
+
+for my $coordinate_node ($kmlFile->findnodes('//*[local-name()="coordinates"]')) {
+    my $text = $coordinate_node->textContent;
+    chomp $text;
+    push @x_y_alt_lines, $text;
+    # print "Pushing... ".$coordinate_node->textContent."\n";
+}
+
+for my $name_node ($kmlFile->findnodes('//*[local-name()="name"]')) {
+    unless($name_node->textContent eq "WAYPOINTS") {
+        my $text = $name_node->textContent;
+        chomp $text;
+        push @name_lines, $text;
+        # print "Pushing... ".$name_node->textContent."\n";
+    }
+}
+
+my @data_keys = qw(dtdid fix_rrr_ddd remarks latitude longitude exp_alt time_from_to time_to_td c1 c2 c3 c4);
+
+my @ext_data_nodes = $kmlFile->findnodes('//*[local-name()="ExtendedData"]');
+
+my @all_node_key_values = ();
+
+my $node_index = 0;
+foreach my $ext_data_node (@ext_data_nodes) {
+    my @data_nodes = $ext_data_node->nonBlankChildNodes();
+
+    push @all_node_key_values, [];
+    my $data_node_key_values = "";
+    foreach my $data_node (@data_nodes) {
+        my @attributes = $data_node->attributes;
+        my $attribute = $attributes[0];
+        $attribute =~ s/name\=\"//;
+        $attribute =~ s/\"//;
+        $attribute =~ s/\s|\//_/g;
+        $attribute =~ s/_//;
+        $attribute = lc $attribute;
+
+        my @value_nodes = $data_node->nonBlankChildNodes();
+        my $value_node = $value_nodes[0];
+
+        my $text_value = $value_node->textContent;
+        $data_node_key_values = $data_node_key_values.$attribute.",".$text_value.",";
+    }
+
+    chop $data_node_key_values;
+    my %node_hash = split(",", $data_node_key_values);
+
+    unless($node_hash{'remarks'}) {
+        $node_hash{'remarks'} = "N/A";
+    }
+
+    $all_node_key_values[$node_index] = \%node_hash;
+    
+    $node_index++;
+}
+
+foreach (@all_node_key_values) {
+    my $node_line = "";
+    foreach my $key (@data_keys) {
+        my $value = $_->{$key} || 'N/A';
+        $node_line = $node_line.",".$value;
+    }
+    $node_line = $node_line."\n";
+    if(substr($node_line, 0, 1) eq ',') {
+        $node_line =~ s/^.//s;
+    }
+    push @data_lines, $node_line;
+}
+
+my $index = 0;
+
+my $csvString = "x,y,altitude,name,dtdid,fix_rrr_ddd,remarks,latitude,longitude,exp_alt,time_from_to,time_to_td,c1,c2,c3,c4\n";
+
+for (@name_lines) {
+    $csvString = $csvString.$x_y_alt_lines[$index].",".$_.",";
+    $csvString = $csvString.$data_lines[$index];
+    $index++;
+}
+
+open my $csvFile, '>', $csvFileName or die "\nCouldn't open file at ./$csvFileName\n";
+print $csvFile $csvString;
+close $csvFile;
